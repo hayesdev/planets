ff68f2fe8874ae4349e2f66aab399da6829211ce
Greg Hayes <gthelight@gmail.com>
2022-10-31 19:10:00 -0600

    refactoring app w/o tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
 import React from "react";
-import { Navbar, Planet, PlanetInfo, Stats } from "./components";
-import { planets } from "./constants";
 
-const App = () => (
-  <div className="flex flex-col justify-start items-center w-full h-screen bg-[#070724] relative">
-    <Navbar />
-    {planets.map((planet) => (
-      <div
-        key={planet.name}
-        className="flex flex-col justify-center w-full bg-[#070724]"
-      >
-        <section id={planet.name} className="flex justify-around w-full">
-          <Planet planet={planet} />
-          <PlanetInfo planet={planet} />
-        </section>
-        <Stats planet={planet} />
-      </div>
-    ))}
-  </div>
-);
+const App = () => <div>hello world</div>;
 
 export default App;
--- src/components/Planet.jsx
+++
@@ -1,9 +0,0 @@
-const Planet = ({ planet }) => (
-  <div className="flex flex-col w-[380px] h-[500px] justify-center items-end mt-[120px] ml-5">
-    <div
-      className={`bg-[${planet.color}] rounded-full w-[290px] h-[290px]`}
-    ></div>
-  </div>
-);
-
-export default Planet;
